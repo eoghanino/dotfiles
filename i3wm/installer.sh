da1797f8e51845b9ec3dbc9faccd097c2ce16666
Eoghan Shields <eoghan.shields@hpe.com>
2018-04-19 17:05:39 +0100

    Added files commented

--- i3wm/installer.sh
+++ i3wm/installer.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 sudo apt-get update;
-sudo apt-get install arandr scrot imagemagick i3 i3lock
+sudo apt-get install arandr scrot imagemagick i3 i3lock rofi
+
+#.config/i3/config
+#.config/i3/i3exit
+#.config/i3status/config
+#.config/i3/i3lock-fancy-multimonitor/ 
